8280389ae79b8b8c508b8c7199b5c1124f498ca2
Oleksandr-Berd <alex.berd86@gmail.com>
2023-11-01 06:03:59 +0200

    header tab is done

--- src/layouts/Header/HeaderStyled.ts
+++ src/layouts/Header/HeaderStyled.ts
@@ -3,4 +3,15 @@ import styled from "@emotion/styled";
 export const HeaderStyled = styled.header`
 padding-top: 40px;
 padding-bottom: 66px;
+
+@media (min-width: 768px){
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+
+    padding-top: 48px;
+    padding-left: 39px;
+    padding-bottom: 77px;
+}
 `
